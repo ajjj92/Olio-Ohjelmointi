c26e8b29d233467d12f18f178913f25479096f79
ajjj92 <atteja92@hotmail.com>
2019-05-30 17:43:50 +0300

    week3 T3 finished

---
+++ Viikko_3/java_week3/T3/T3/src/Mainclass.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Atte
+ */
+public class Mainclass {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        BottleDispenser PulloMaatti = new BottleDispenser();
+        
+        
+        PulloMaatti.addMoney();
+        PulloMaatti.buyBottle();
+        PulloMaatti.buyBottle();
+        PulloMaatti.addMoney();
+        PulloMaatti.addMoney();
+        PulloMaatti.buyBottle();
+        PulloMaatti.returnMoney();
+    }
+    
+}
